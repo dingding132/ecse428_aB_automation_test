55c7983cad0f1794e862fe9409b6f709b9a17ea2
Fiona Hang <fiona.hang@mail.mcgill.ca>
2019-03-06 18:13:53 -0500

    add email recipient and send tests

--- src/test/java/com/ecse428/StepDefinition/ImageAttachmentOutlook.java
+++ src/test/java/com/ecse428/StepDefinition/ImageAttachmentOutlook.java
@@ -8,6 +8,7 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 import java.io.File;
+import java.util.Random;
 
 import org.junit.Assert;
 
@@ -19,14 +20,22 @@ import org.openqa.selenium.WebElement;
 
 public class ImageAttachmentOutlook {
 
-    private final String PATH_TO_CHROME_DRIVER = "/Users/qingqing/Documents/workspace/School/ECSE428/chromedriver";
+    // private final String PATH_TO_CHROME_DRIVER = "/Users/qingqing/Documents/workspace/School/ECSE428/chromedriver";
+    private final String PATH_TO_CHROME_DRIVER = "/Users/fionahang/Documents/MCGILL/U4/ECSE428/chromedriver";
     private final String OUTLOOK_SIGNIN_URL = "https://outlook.live.com/owa/?nlp=1";
     private final String OUTLOOK_INBOX_TITLE = "Mail - Fiona Wang - Outlook";
-    private final String EMAIL_ADDRESS = "fionawang.ecse428@hotmail.com";
-    private final String EMAIL_PASSWORD = "ecse4282019";
+    // private final String EMAIL_ADDRESS = "fionawang.ecse428@hotmail.com";
+    // private final String EMAIL_PASSWORD = "ecse4282019";
+    private final String EMAIL_ADDRESS = "fionawang.ecse429@hotmail.com";
+    private final String EMAIL_PASSWORD = "ecse4292019";
     private final String IMAGE_FILE_DIRECTORY = "src/test/resources/";
+    private final String RECIPIENT_EMAIL = "fiona.hang96@gmail.com";
+    private final String RECIPIENT_EMAIL_2 = "dongwen.wang@gmail.com";
 
-    private final String NEW_MSG_BTN_ID = "id__3";
+    private String emailSubject;
+
+    // private final String NEW_MSG_BTN_ID = "id__3";
+    private final String NEW_MSG_BTN_ID = "id__5";
     private final String EMAIL_FORM_ID = "i0116";
     private final String PASS_FORM_ID = "i0118";
     private final String SIGNIN_BTN_ID = "idSIButton9";
@@ -34,6 +43,12 @@ public class ImageAttachmentOutlook {
     private final String FILE_INPUT_CLASSNAME = "_1iWL2ddLCtiEp9P-UVh8nV";
     private final String UPLOADED_FILE_CLASSNAME = "is-fadeIn";
     private final String ERROR_MSG_CLASSNAME = "_3c57Fvb9-GOxD8a2WZ2cw-";
+    private final String RECIPIENT_FORM_CLASSNAME = "pickerInput_269bfa71";
+    private final String RECIPIENT_DROPDOWN_ID = "sug-header-item2";
+    private final String SUBJECT_FORM_ID = "subjectLine0";
+    private final String SEND_BTN_CLASSNAME = "_3wi0Ec22JNBf3ClMCpnf1Z";
+    private final String SENT_ITEMS_BTN_XPATH = "//span[text()='Sent Items']";
+    private final String EMAIL_TITLE_SPAN_XPATH = "//div[contains(@class,'RKJYnFQ991LYsw_styUw')]/span[text()='" + emailSubject + "']";
 
     private final int TIME_WAIT_FOR_CONDITION = 15;
 
@@ -82,14 +97,60 @@ public class ImageAttachmentOutlook {
         }
     }
 
-    @And("^I have selected a single email recipient$")
+    @And("^I have added a single email recipient$")
     public void enter_single_email_recipient() throws Throwable {
         System.out.println("Step: Enter email recipient");
+
+        // Input the recipient's email address
+        try {
+            // Input the email address
+            WebElement emailForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(RECIPIENT_FORM_CLASSNAME)));
+            emailForm.sendKeys(RECIPIENT_EMAIL);
+            // Select the email address in the drop down
+            WebElement selectEmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(RECIPIENT_DROPDOWN_ID)));
+            selectEmail.click();
+        } catch (Exception e) {
+            printErrorMsg(e);
+        }
     }
 
-    @And("^I have selected a two email recipient$")
+    @And("^I have added two email recipients$")
     public void enter_two_email_recipients() throws Throwable {
         System.out.println("Step: Enter two email recipients");
+
+        // Input both recipient's email address
+        try {
+            // Input the email address
+            WebElement emailForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(RECIPIENT_FORM_CLASSNAME)));
+            emailForm.sendKeys(RECIPIENT_EMAIL);
+            // Select the email address in the drop down
+            WebElement selectEmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(RECIPIENT_DROPDOWN_ID)));
+            selectEmail.click();
+            // Input the second email address
+            WebElement emailForm2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(RECIPIENT_FORM_CLASSNAME)));
+            emailForm2.sendKeys(RECIPIENT_EMAIL_2);
+            // Select the email address in the drop down
+            WebElement selectEmail2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(RECIPIENT_DROPDOWN_ID)));
+            selectEmail2.click();
+        } catch (Exception e) {
+            printErrorMsg(e);
+        }
+    }
+
+    @And("^I have written a subject$")
+    public void enter_email_subject() throws Throwable {
+        System.out.println("Step: Enter email subject");
+        try {
+            emailSubject = generateRandomString(10);
+            System.out.println("Email Subject: " + emailSubject);
+
+            // Input email subject
+            WebElement subjectInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(SUBJECT_FORM_ID)));
+            subjectInput.sendKeys(emailSubject);
+        } catch (Exception e) {
+            printErrorMsg(e);
+        }
+
     }
 
     @When("^I attach a .png image$")
@@ -123,11 +184,36 @@ public class ImageAttachmentOutlook {
     @And("^I click on “send” button$")
     public void click_send_button() throws Throwable {
         System.out.println("Step: Click on send button");
+
+        try {
+            // Click on send button to send the email
+            WebElement sendButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(SEND_BTN_CLASSNAME)));
+            sendButton.click();
+        } catch (Exception e) {
+            printErrorMsg(e);
+        }
     }
 
     @Then("^I should be able to see the email in my “Sent” folder$")
     public void verify_email_sent() throws Throwable {
         System.out.println("Step: Verify email is sent");
+
+        try {
+            // Go to Sent Items page
+            WebElement sentItemsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SENT_ITEMS_BTN_XPATH)));
+            sentItemsButton.click();
+
+            // Get the title of the last sent email
+            WebElement emailTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EMAIL_TITLE_SPAN_XPATH)));
+            String sentEmailTitle = emailTitle.getText();
+            System.out.println("Last email sent: " + sentEmailTitle);
+
+            Assert.assertTrue(sentEmailTitle.equals(emailSubject));
+
+        } catch (Exception e) {
+            printErrorMsg(e);
+        }
+
     }
 
     @Then("^a file too large error message appears$")
@@ -135,8 +221,7 @@ public class ImageAttachmentOutlook {
         System.out.println("Step: Verify file too large error message appears");
         // Find error message and assert it
         try {
-            WebElement errMsg = wait
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.className(ERROR_MSG_CLASSNAME)));
+            WebElement errMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(ERROR_MSG_CLASSNAME)));
             Assert.assertTrue(errMsg.isEnabled());
         } catch (Exception e) {
             printErrorMsg(e);
@@ -223,6 +308,20 @@ public class ImageAttachmentOutlook {
         } catch (Exception e) {
             printErrorMsg(e);
         }
+    }
+
+    /** Generates a random string made of alphabetical letters **/
+    private String generateRandomString(int targetStringLength) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+        return generatedString;
     }
 
     /** Print comprehensible error message */
--- src/test/resources/ImageAttachmentOutlook.feature
+++ src/test/resources/ImageAttachmentOutlook.feature
@@ -2,34 +2,39 @@ Feature: Hotmail send email with image attachment
 
   Scenario: Attaching an image to email with one recipient
     Given I am on the new message page
-    And I have selected a single email recipient
+    And I have added a single email recipient
+    And I have written a subject
     When I attach a .png image
     And I click on “send” button
     Then I should be able to see the email in my “Sent” folder
 
   Scenario: Attaching two images to email with one recipient
     Given I am on the new message page
-    And I have selected a single email recipient
+    And I have added a single email recipient
+    And I have written a subject
     When I attach two .png images
     And I click on “send” button
     Then I should be able to see the email in my “Sent” folder
 
   Scenario: Attaching an image to email with two recipients
     Given I am on the new message page
-    And I have selected a two email recipient
+    And I have added two email recipients
+    And I have written a subject
     When I attach a .png image
     And I click on “send” button
     Then I should be able to see the email in my “Sent” folder
 
   Scenario: Attaching a jpeg image to email with one recipient
     Given I am on the new message page
-    And I have selected a single email recipient
+    And I have added a single email recipient
+    And I have written a subject
     When I attach a .jpeg image
     And I click on “send” button
     Then I should be able to see the email in my “Sent” folder
 
   Scenario: Attaching a image over 33MB to email with one recipient
     Given I am on the new message page
-    And I have selected a single email recipient
+    And I have added a single email recipient
+    And I have written a subject
     When I attach a .png image over 33MB
     Then a file too large error message appears
